9695f54a230a77e3eb6006be017967bc812b2c49
Shakil Abdin <shakil.abdin@gmail.com>
2020-10-06 17:16:06 -0400

    done

--- main.go
+++ main.go
@@ -5,27 +5,27 @@ import (
 	"sort"
 )
 
-type Person struct {
-	First string
-	Age   int
+type person struct {
+	first string
+	age   int
 }
 
-type ByName []Person
+type byName []person
 
-func (bn ByName) Len() int           { return len(bn) }
-func (bn ByName) Swap(i, j int)      { bn[i], bn[j] = bn[j], bn[i] }
-func (bn ByName) Less(i, j int) bool { return bn[i].First < bn[j].First }
+func (bn byName) Len() int           { return len(bn) }
+func (bn byName) Swap(i, j int)      { bn[i], bn[j] = bn[j], bn[i] }
+func (bn byName) Less(i, j int) bool { return bn[i].first < bn[j].first }
 
 func main() {
-	p1 := Person{"James", 32}
-	p2 := Person{"Moneypenny", 27}
-	p3 := Person{"Q", 64}
-	p4 := Person{"M", 56}
+	p1 := person{"James", 32}
+	p2 := person{"Moneypenny", 27}
+	p3 := person{"Q", 64}
+	p4 := person{"M", 56}
 
-	people := []Person{p1, p2, p3, p4}
+	people := []person{p1, p2, p3, p4}
 
 	fmt.Println(people)
-	sort.Sort(ByName(people))
+	sort.Sort(byName(people))
 	fmt.Println(people)
 
 }
